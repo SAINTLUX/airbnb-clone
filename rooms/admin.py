c62a3c5c4c0fc66c1742d4a020b94bf659532425
saintluxpgw <saintluxpgw@gmail.com>
2020-11-27 18:50:46 +0900

    room admin function added

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,9 +15,25 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
     list_display = (
         "name",
-        "host",
         "country",
         "city",
         "price",
@@ -28,11 +44,26 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
     )
 
-    list_filter = ("instant_book", "city", "country")
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
 
     search_fields = ("=city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
 
 
 @admin.register(models.Photo)
--- rooms/models.py
+++ rooms/models.py
@@ -39,7 +39,7 @@ class Facility(AbstractItem):
     """ Facility Model Definiton """
 
     class Meta:
-        verbose_name_plural = "Facilites"
+        verbose_name_plural = "Facilities"
 
 
 class HouseRule(AbstractItem):
@@ -82,7 +82,7 @@ class Room(core_models.TimeStampedModel):
     host = models.ForeignKey("users.User", on_delete=models.CASCADE)
     room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
     amenities = models.ManyToManyField("Amenity", blank=True)
-    facility = models.ManyToManyField("Facility", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
     house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
