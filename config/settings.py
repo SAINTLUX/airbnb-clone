cda218942a8c60b0c1a22b7ec73ce35a81c86b28
saintluxpgw <saintluxpgw@gmail.com>
2020-11-27 16:42:03 +0900

    review model, reservation model admin custom

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ PROJECT_APPS = [
     "core.apps.CoreConfig",
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
+    "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
