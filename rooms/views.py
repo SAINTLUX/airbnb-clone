92082bef950b301bca319750a08e2f124660a767
saintluxpgw <saintluxpgw@gmail.com>
2020-12-18 19:52:41 +0900

    view and urls a basic test

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
+from datetime import datetime
 from django.shortcuts import render
 
-# Create your views here.
+
+def all_rooms(request):
+    now = datetime.now()
+    hungry = True
+    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
