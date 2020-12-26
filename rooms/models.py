4bab3d56c1e36f6af83c09e6ed31a79cd8e389a1
saintluxpgw <saintluxpgw@gmail.com>
2020-12-26 16:12:42 +0900

    get_absolute_url to room-detail FBV

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
 
@@ -108,6 +109,9 @@ class Room(core_models.TimeStampedModel):
         # Temporary for failover
         return all_ratings / len(all_reviews)
     """
+
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={"pk": self.pk})
 
     def total_rating(self):
         all_reviews = self.reviews.all()
